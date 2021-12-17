f73a4e3eb22e395fbc274f46d9fc9932cfea87bf
zhang-tang <2907578142@qq.com>
2021-12-17 11:06:45 +0800

    zt

--- LiquidLevel.ts
+++ LiquidLevel.ts
@@ -1,6 +1,6 @@
 
 //% weight=100 color=#0fbc11 icon="\uf0b2"
-namespace Liquid {
+namespace LiquidLevel {
 
     //% block="pin %pin liquid level reached a certain height？"
     export function isLiquid(pin: DigitalPin): number {
